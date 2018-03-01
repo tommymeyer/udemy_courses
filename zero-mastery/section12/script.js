211956cfaea4f0a981d9b2348a684dc7147670c6
tommymeyer <tlmeyer942@gmail.com>
2018-03-01 14:52:17 -0700

    Adds completed section 11 and beginnings of section 12.

---
+++ zero-mastery/section12/script.js
@@ -0,0 +1,38 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelector("li");
+
+function inputLength() {
+  return input.value.length;
+};
+
+function createListEl() {
+  var li = document.createElement("li");
+
+  li.appendChild(document.createTextNode(input.value));
+
+  ul.appendChild(li);
+
+  input.value = "";
+};
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListEl();
+  }
+};
+
+function addListAfterKeyPress(e) {
+  if (inputLength() > 0 && e.which === 13) {
+    createListEl();
+  }
+};
+
+li.addEventListener("click", function() {
+  li.classList.toggle("done");
+});
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keyup", addListAfterKeyPress);
