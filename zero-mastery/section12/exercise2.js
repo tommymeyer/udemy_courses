1eeb1237f3344b7dc0a14ebd05d0c17968c4ba43
tommymeyer <tlmeyer942@gmail.com>
2018-05-06 15:44:42 -0700

    Redoing section 12 for review purposes.

--- zero-mastery/section12/exercise2.js
+++ zero-mastery/section12/exercise2.js
@@ -1,60 +1,35 @@
-// Lecture 106
-// Exercise #2
+// // Section 12 Lecture 107
+// //#1 change this function into a ternary and assign it to variable called experiencePoints
+// var experiencePoints = (winBattle()) ? 10 : 1;
 
-//#1 change this function into a ternary and assign it to variable called experiencePoints
+// //Using this function, answer the questions below:
+function moveCommand(direction) { 
+  var whatHappens;
+  switch(direction) {
+    case "forward":
+    whatHappens = "you encounter a monster";
+      break;
+    case "back":
+    whatHappens = "you arrived home";
+      break;
+    case "right":
+    whatHappens = "you found a river";
+      break;
+    case "left":
+    whatHappens = "you run into a troll";
+      break;
+    default:
+    whatHappens = "please enter a valid direction";
+  }
+  return whatHappens;
+}
 
-// function winBattle(bool) {
-//     return bool;
-// }
-
-// var experiencePoints =  (winBattle(false)) ? 10 : 1;
-
-//Using this function, answer the questions below:
-// function moveCommand(direction) {
-//     var whatHappens;
-//     switch (direction) {
-//         case "forward":
-//             whatHappens = "you encounter a monster";
-//             break;
-//         case "back":
-//             whatHappens = "you arrived home";
-//             break;
-//         case "right":
-//             whatHappens = "you found a river";
-//             break;
-//         case "left":
-//             whatHappens = "you run into a troll";
-//             break;
-//         default:
-//             whatHappens = "please enter a valid direction";
-//     }
-//     return whatHappens;
-// }
-
-//#2 return value when moveCommant("forward");
-
-//#3 return value when moveCommant("back");
-
-//#4 return value when moveCommant("right");
-
-//#5 return value when moveCommant("left");
-
-//BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
-
-// function rpg(roll) {
-//     var result = prompt();
-//     switch (roll) {
-//         case "1":
-//             result = "You've been killed.";
-//             break;
-//         case "2":
-//             result = "You've been terribly injured.";
-//             break;
-//         case "3":
-//             result = "You only just dodged the attack.";
-//             break;
-//         default:
-//             result = "Roll the die to decide your fate.";
-//     }
-//     return result;
-// }
+// //#2 return value when moveCommand("forward");
+moveCommand("forward");
+// //#3 return value when moveCommand("back");
+moveCommand("back");
+// //#4 return value when moveCommand("right");
+moveCommand("right");
+// //#5 return value when moveCommand("left");
+moveCommand("left");
+// //BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
