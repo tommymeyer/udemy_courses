bdc4ab1d02f88056455ca3f24fb3dd6dd2b1ff8f
tommymeyer <tlmeyer942@gmail.com>
2018-05-13 23:05:41 -0700

    Adds redos for exercises 3-5.

---
+++ zero-mastery/section12/exercise3.js
@@ -0,0 +1,64 @@
+// // change everything below to the newer Javascript!
+
+// // let + const
+// var a = 'test';
+// var b = true;
+// var c = 789;
+// a = test;
+// let a = 'test';
+// let a = "test";
+// let b = true;
+// let c = 789;
+// let a = test;
+
+// // Destructuring
+// var person = {
+//     firstName : "John",
+//     lastName  : "Doe",
+//     age       : 50,
+//     eyeColor  : "blue"
+// };
+
+// var firstName = person.firstName;
+// var lastName = person.lastName;
+// var age = person.age;
+// var eyeColor = person.eyeColor;
+// let { firstName, lastName, age, eyeColor } = person;
+
+
+// // Object properties
+// var a = 'test';
+// var b = true;
+// var c = 789;
+
+// var okObj = { 
+//   a,
+//   b,
+//   c
+// };
+
+
+// // Template strings
+// var message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
+
+
+// // default arguments
+// // default age to 10;
+// function isValidAge(age="10") {
+//     return age
+// }
+
+// // Symbol
+// // Create a symbol: "This is my first Symbol"
+// let sym = Symbol("This is my first Symbol.");
+
+// // Arrow functions
+// function whereAmI(username, location) {
+//     if (username && location) {
+//         return "I am not lost";
+//     } else {
+//         return "I am totally lost!";
+//     }
+// }
+
+// const whereAmI = (username, location) => (username && location) ? "I am not lost" : "I am totally lost!";
---
+++ zero-mastery/section12/exercise5.js
@@ -0,0 +1,75 @@
+// // Complete the below questions using this array:
+const array = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"]
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"]
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"]
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"]
+  },
+
+];
+
+// //Create an array using forEach that has all the usernames with a "!" to each of the usernames
+// const newArray = [];
+
+// array.forEach((user) => {
+//   let { username } = user;
+//   newArray.push(username + "!");
+// });
+
+// console.log(newArray);
+
+
+// //Create an array using map that has all the usernames with a "? to each of the usernames
+// const mapArray = array.map((user) => {
+//   let { username } = user;
+//   username = username + "?";
+//   return username;
+// });
+
+// console.log(mapArray);
+
+// //Filter the array to only include users who are on team: red
+
+
+
+// //Find out the total score of all users using reduce
+// const reduceArray = array.reduce((acc, user) => {
+//   return acc + user.score;
+// }, 0);
+
+// console.log(reduceArray);
+
+
+// // (1), what is the value of i?
+// // (2), Make this map function pure:
+// const arrayNum = [1, 2, 4, 5, 8, 9];
+// const newArray = arrayNum.map((num, i) => {
+//   return num * 2;
+// });
+
+// console.log(newArray);
+
+
+
+
+// //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+
