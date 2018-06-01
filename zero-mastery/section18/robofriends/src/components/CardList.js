040ff8e646f44acbdb6c358626ed6af12e8045b3
tommymeyer <tlmeyer942@gmail.com>
2018-06-01 16:38:14 -0700

    Initializes Robofriends2 for redo purposes.

--- zero-mastery/section18/robofriends/src/components/CardList.js
+++ zero-mastery/section18/robofriends/src/components/CardList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  return(
+  return (
     <div>
       {
         robots.map((user, i) => {
--- zero-mastery/section18/robofriends/src/components/SearchBox.js
+++ zero-mastery/section18/robofriends/src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const SearchBox = ({ searchfield, searchChange }) => {
-  return(
+  return (
     <div className='pa2'>
       <input
         className='pa3 ba b--green bg-lightest-blue'
