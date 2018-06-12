2cc5161e3e0b7e83b4726d016838de799ebdcd3d
tommymeyer <tlmeyer942@gmail.com>
2018-06-11 19:31:40 -0700

    Adds completed exercises for sections 19 and 21.

--- zero-mastery/section19/exerciseAsyncAwait.js
+++ zero-mastery/section19/exerciseAsyncAwait.js
@@ -1,9 +1,18 @@
 // Solve the below problems:
 
 // #1) Convert the below promise into async await
-fetch('https://swapi.co/api/starships/9/')
-  .then(response => response.json())
-  .then(console.log)
+// fetch('https://swapi.co/api/starships/9/')
+//   .then(response => response.json())
+//   .then(console.log)
+
+
+async function getFetch() {
+  const resp = await fetch('https://swapi.co/api/starships/9/');
+  const data = await resp.json();
+  console.log(data);
+}
+
+// getFetch();
 
 
 // #2) ADVANCED: Update the function below from the video to also have
@@ -17,13 +26,32 @@ const urls = [
 ]
 
 const getData = async function() {
-  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+  try {
+    const [users, posts, albums] = await Promise.all(urls.map(url =>
       fetch(url).then(resp => resp.json())
-  ));
-  console.log('users', users);
-  console.log('posta', posts);
-  console.log('albums', albums);
+    ));
+    console.log('users', users);
+    console.log('posts', posts);
+    console.log('albums', albums);
+  } catch(err) {
+    console.log('whoops', err);
+  }
 }
+
+getData();
+
+// const getData = async function() {
+//   const resp = urls.map(async function(url) {
+//     const data = await fetch(url);
+//     const result = await data.json();
+//     return result;
+//   });
+//   console.log('users', urls[0]);
+//   console.log('posts', urls[1]);
+//   console.log('albums', urls[2]);
+// }
+
+// getData();
 
 // #3) Add a try catch block to the #2 solution in order to catch any errors.
 // Now chnage one of the urls so you console.log your error with 'ooooooops'
--- zero-mastery/section21/scripts.js
+++ zero-mastery/section21/scripts.js
@@ -1,43 +1,22 @@
-const data = fetch('https://swapi.co/api/people/1/')
-  .then((response) => response.json())
-  .then((myJson) => {
-    console.log(myJson);
+const ul = document.getElementById('star_wars_list');
+const url = 'https://swapi.co/api/people/1/';
+
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+fetch(url)
+  .then(resp => resp.json())
+  .then(info => {
+    let person = Object.values(info);
+    person.map(value => {
+      let li = createNode('li');
+      li.innerHTML = value;
+      append(ul, li);
+    })
+    .catch(error => console.log('error'))
   });
-
-// document.addEventListener('DOMContentLoaded', loadPlanets);
-
-// function loadPlanets() {
-//   var docBod = document.body;
-//   for (var j = 1; j < 8; j++) {
-//     (function (i) { //need to create closure for j
-//       var f = i;
-//       var req = new XMLHttpRequest();
-//       var URLhost = 'https://swapi.co/api/planets/?page=' + f;
-//       req.open('GET', URLhost, true);
-//       req.addEventListener('load', function () {
-//         if (req.status >= 200 && req.status < 400) {
-//           var response = JSON.parse(req.responseText);
-//           console.log(response);
-//           var planetHead = document.createElement('h5');
-//           docBod.appendChild(planetHead);
-//           planetHead.textContent = 'Planets Page - ' + f;
-//           var planetList = document.createElement('ol');
-//           planetHead.appendChild(planetList);
-
-//           for (var k = 0; k < response.results.length; k++) {
-//             (function (y) {
-//               var planetIn = document.createElement('li');
-//               planetIn.textContent = response.results[y].name;
-//               planetList.appendChild(planetIn);
-//             })(k);
-//           }
-
-//         } else {
-//           console.log('Error in network request: ' + req.statusText);
-//         }
-//       });
-//       req.send(null);
-//       event.preventDefault();
-//     })(j);
-//   }
-// }
